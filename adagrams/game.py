3b89b45cf9866b7bc8c007130ebbbd72a7f134cf
Maya Zakharova <mayzakharova@gmail.com>
2025-09-11 23:44:09 -0500

    add functionality for score_word(word)

--- adagrams/game.py
+++ adagrams/game.py
@@ -29,6 +29,35 @@ LETTER_POOL = {
     'Z': 1
 }
 
+LETTER_SCORE = {
+    "A": 1,
+    "E": 1,
+    "I": 1,
+    "O": 1,
+    "U": 1,
+    "L": 1,
+    "N": 1,
+    "R": 1,
+    "S": 1, 
+    "T": 1,
+    "D": 2, 
+    "G": 2,
+    "B": 3, 
+    "C": 3, 
+    "M": 3, 
+    "P": 3,
+    "F": 4, 
+    "H": 4, 
+    "V": 4, 
+    "W": 4, 
+    "Y": 4,
+    "K": 5,
+    "J": 8, 
+    "X": 8,
+    "Q": 10,
+    "Z": 10
+}
+
 def draw_letters():
     all_letters = []
     hand = []
@@ -48,7 +77,7 @@ def uses_available_letters(word, letter_bank):
 
     if len(word) > len(letter_bank):
         return False
-    
+        
     for letter in word.upper():
         if letter in bank_copy:
             bank_copy.remove(letter)
@@ -58,7 +87,17 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+    score = 0
+    upper_word = word.upper()
+
+    for letter in upper_word:
+        score += LETTER_SCORE[letter]
+
+    if len(upper_word) > 6:
+        score += 8
+
+    return score
+
 
 def get_highest_word_score(word_list):
     pass
