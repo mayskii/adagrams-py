b10e57f21c15867e38933293df0f9f35860f9e23
Maya Zakharova <mayzakharova@gmail.com>
2025-09-11 22:41:38 -0500

    add functionality for draw_letters()

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,46 @@
 from random import randint
 
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
 def draw_letters():
-    pass
+    all_letters = []
+    for letter, count in LETTER_POOL.items():
+        for _ in range(0, count):
+            all_letters.append(letter)
+
+    hand = []
+    for _ in range(10):
+        index = randint(0, len(all_letters) - 1)
+        hand.append(all_letters[index])
+        all_letters.pop(index)
+    return hand
 
 def uses_available_letters(word, letter_bank):
     pass
